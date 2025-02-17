7dd567edc92273559101ab4c0b5f51cd631e0781
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2025-02-17 05:41:22 +0400

    [restate]: setup nativewind

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,19 +1,20 @@
-import { Text, View } from "react-native";
+import {Text, View} from "react-native";
 import {Link} from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-        <Link href="/sign-in">Sign In</Link>
-        <Link href="/explore">Explore</Link>
-        <Link href="/profile">Profile</Link>
-        <Link href="/properties/1">Property</Link>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text className="font-bold text-lg my-10">welcome to reState</Text>
+            <Link href="/sign-in">Sign In</Link>
+            <Link href="/explore">Explore</Link>
+            <Link href="/profile">Profile</Link>
+            <Link href="/properties/1">Property</Link>
+        </View>
+    );
 }
